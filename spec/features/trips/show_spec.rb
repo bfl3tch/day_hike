8011a4f4c9bf6fd0275a9b0cf09fa08d3656d559
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-24 10:59:47 -0400

    User story 2 complete

--- spec/features/trips/show_spec.rb
+++ spec/features/trips/show_spec.rb
@@ -1,20 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'The trips index page' do
+RSpec.describe 'The trips show page' do
   before(:each) do
     @trip1 = Trip.create!(name: 'summer')
     @trip2 = Trip.create!(name: 'spring break')
     @trip3 = Trip.create!(name: 'thanksgiving break')
     @trip4 = Trip.create!(name: 'winter break')
     @trip5 = Trip.create!(name: '3 day weekend')
-    visit '/trips'
+    visit "/trips/#{@trip1.id}"
   end
   it 'displays a list of all hiking trip names' do
-    save_and_open_page
     expect(page).to have_content(@trip1.name)
-    expect(page).to have_content(@trip2.name)
-    expect(page).to have_content(@trip3.name)
-    expect(page).to have_content(@trip4.name)
-    expect(page).to have_content(@trip5.name)
+    expect(page).to_not have_content(@trip2.name)
   end
 end
